b18549283e9c41dd7397ddb2492f0ba67ba8d490
piero-san <razudira282@gmail.com>
2022-07-06 12:15:41 +0700

    add img data and boxed characters

--- tesseract.py
+++ tesseract.py
@@ -1,6 +1,7 @@
 import pytesseract
 import argparse
 import cv2
+from preprocessing import *
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Image path")
@@ -9,6 +10,26 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+gray = get_grayscale(image)
+threshold = thresholding(gray)
+opening = opening(gray)
+canny = canny(gray)
+
+# images = [image, gray, threshold, opening, canny]
+images = [image]
+
 custom_config = r"--oem 3 --psm 6"
-text = pytesseract.image_to_string(image, config=custom_config)
-print(text)
+
+for i in images:
+    # text = pytesseract.image_to_string(i, config=custom_config)
+    # print(text)
+    # print("----")
+    h, w, c = i.shape
+    boxes = pytesseract.image_to_boxes(i)
+    for b in boxes.splitlines():
+        b = b.split(" ")
+        i = cv2.rectangle(
+            i, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2
+        )
+    cv2.imshow("img", i)
+    cv2.waitKey(0)
