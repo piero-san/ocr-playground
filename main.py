b180fd60b79e265492e39a26fa60e33d533a7938
piero-san <razudira282@gmail.com>
2022-07-06 12:20:01 +0700

    feat: init trocrp

---
+++ main.py
@@ -0,0 +1,13 @@
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+from PIL import Image
+import requests
+
+url = "https://fki.tic.heia-fr.ch/static/img/a01-122-02-00.jpg"
+image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
+
+processor = TrOCRProcessor.from_pretrained("microsoft/trocr-large-printed")
+model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-large-printed")
+pixel_values = processor(images=image, return_tensors="pt").pixel_values
+
+generated_ids = model.generate(pixel_values)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
